7b5117cff4b9ca23f42280d616d6fd831fd885ae
Yisu Wang <ywang30@cs.tufts.edu>
2017-02-19 21:30:52 -0500

    big run on opts

--- exp/Dum.hs
+++ exp/Dum.hs
@@ -1,37 +1,24 @@
 {-# LANGUAGE BangPatterns #-}
 module Dum where
+import System.Environment
 
-returnM :: x -> Maybe x
-returnM x = Just x
-
-eachM :: Maybe x -> (x -> y) -> Maybe y
-eachM ((!(Just x))) (!f) = Just (f x)
-eachM Nothing (!f) = Nothing
-
-thenM :: Maybe x -> (x -> Maybe y) -> Maybe y
-thenM ((!(Just x))) kM = kM x
-thenM Nothing (!kM) = Nothing
-
-failM :: Maybe x
-failM = Nothing
-
-orM :: Maybe x -> Maybe x -> Maybe x
-orM (Just x) yM = Just x
-orM Nothing (!yM) = yM
-
-guardM :: Bool -> Maybe x -> Maybe x
-guardM (!b) (!xM) = if b then xM else failM
-
-filterM :: (x -> Bool) -> Maybe x -> Maybe x
-filterM p xM = xM `thenM` (\ (!x) -> p x `guardM` returnM x)
-
-theM :: Maybe x -> x
-theM (!((!(Just x)))) = x
-
-existsM :: Maybe x -> Bool
-existsM ((!(Just x))) = True
-existsM Nothing = False
-
-useM :: x -> Maybe x -> x
-useM xfail ((!(Just (!x)))) = x
-useM xfail (!Nothing) = xfail
+integrate1D :: Double -> Double -> (Double -> Double) -> Double
+integrate1D (!l) u (!f)
+  = let (!d) = (u - l) / 8.0 in
+      d *
+        sum
+          [(f l) * 0.5, f (l + d), f (l + (2.0 * d)), f (l + (3.0 * d)),
+           f (l + (4.0 * d)), f (u - (3.0 * d)), f (u - (2.0 * d)), f (u - d),
+           (f u) * 0.5]
+integrate2D l1 u1 (!l2) (!u2) f
+  = integrate1D l2 u2 (\ (!y) -> integrate1D l1 u1 (\ x -> f x y))
+zark u v = integrate2D 0.0 u 0.0 v (\ x -> (\ y -> x * y))
+ints = [1.0 ..] :: [Double]
+zarks = zipWith zark ints (map (2.0 *) ints)
+rtotals = head zarks : zipWith (+) (tail zarks) rtotals
+rtotal n = rtotals !! n
+is = map (^ 4) ints
+itotals = head is : zipWith (+) (tail is) itotals
+itotal (!n) = itotals !! n
+es = map (^ 2) (zipWith (-) rtotals itotals)
+etotal (!n) = sum (take n es)
--- exp/Dumrn.hs
+++ exp/Dumrn.hs
@@ -1,31 +1,35 @@
 {-# LANGUAGE BangPatterns #-}
 module Dum where
-returnM :: x_azx -> Maybe x_azx
-returnM x_azy = Just x_azy
-eachM :: Maybe x_azv -> (x_azv -> y_azw) -> Maybe y_azw
-eachM ((!(Just x_azz))) (!f_azA) = Just (f_azA x_azz)
-eachM Nothing (!f_azB) = Nothing
-thenM :: Maybe x_azt -> (x_azt -> Maybe y_azu) -> Maybe y_azu
-thenM ((!(Just x_azC))) kM_azD = kM_azD x_azC
-thenM Nothing (!kM_azE) = Nothing
-failM :: Maybe x_azs
-failM = Nothing
-orM :: Maybe x_azr -> Maybe x_azr -> Maybe x_azr
-orM (Just x_azF) yM_azG = Just x_azF
-orM Nothing (!yM_azH) = yM_azH
-guardM :: Bool -> Maybe x_azq -> Maybe x_azq
-guardM (!b_azI) (!xM_azJ) = if b_azI then xM_azJ else failM
-filterM :: (x_azp -> Bool) -> Maybe x_azp -> Maybe x_azp
-filterM p_azK xM_azL
-  = xM_azL
-    `thenM`
-      (\ (!x_azM) -> p_azK x_azM `guardM` returnM x_azM)
-theM :: Maybe x_azo -> x_azo
-theM (!((!(Just x_azN)))) = x_azN
-existsM :: Maybe x_asj -> Bool
-existsM ((!(Just x_azO))) = True
-existsM Nothing = False
-useM :: x_asi -> Maybe x_asi -> x_asi
-useM xfail_azP ((!(Just (!x_azQ)))) = x_azQ
-useM xfail_azR (!Nothing) = xfail_azR
+import System.Environment
+integrate1D :: Double -> Double -> (Double -> Double) -> Double
+integrate1D (!l_azc) u_azd (!f_aze)
+  = let (!d_azf) = (u_azd - l_azc) / 8.0
+    in
+      d_azf
+      * sum
+          [(f_aze l_azc) * 0.5, f_aze (l_azc + d_azf),
+           f_aze (l_azc + (2.0 * d_azf)), f_aze (l_azc + (3.0 * d_azf)),
+           f_aze (l_azc + (4.0 * d_azf)), f_aze (u_azd - (3.0 * d_azf)),
+           f_aze (u_azd - (2.0 * d_azf)), f_aze (u_azd - d_azf),
+           (f_aze u_azd) * 0.5]
+integrate2D l1_a12X u1_a12Y (!l2_a12Z) (!u2_a130) f_a131
+  = integrate1D
+      l2_a12Z
+      u2_a130
+      (\ (!y_a132)
+         -> integrate1D
+              l1_a12X u1_a12Y (\ x_a133 -> f_a131 x_a133 y_a132))
+zark u_a134 v_a135
+  = integrate2D
+      0.0 u_a134 0.0 v_a135 (\ x_a136 -> (\ y_a137 -> x_a136 * y_a137))
+ints = [1.0 .. ] :: [Double]
+zarks = zipWith zark ints (map (2.0 *) ints)
+rtotals
+  = head zarks : zipWith (+) (tail zarks) rtotals
+rtotal n_a16O = rtotals !! n_a16O
+is = map (^ 4) ints
+itotals = head is : zipWith (+) (tail is) itotals
+itotal (!n_a16P) = itotals !! n_a16P
+es = map (^ 2) (zipWith (-) rtotals itotals)
+etotal (!n_a16Q) = sum (take n_a16Q es)
 
