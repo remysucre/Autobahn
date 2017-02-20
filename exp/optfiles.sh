d499efccf9db1398f5740b7e4dec29a5112b8797
Yisu Wang <ywang30@cs.tufts.edu>
2017-02-20 16:30:19 -0500

    run

---
+++ exp/optfiles.sh
@@ -0,0 +1,13 @@
+stack exec exp ~/nofib/imaginary/bernouilli/Main.hs.opt &&
+stack exec exp ~/nofib/imaginary/exp3_8/Main.hs.opt &&
+stack exec exp ~/nofib/imaginary/gen_regexps/Main.hs.opt &&
+stack exec exp ~/nofib/imaginary/integrate/Main.hs.opt &&
+stack exec exp ~/nofib/imaginary/kahan/Main.hs.opt &&
+stack exec exp ~/nofib/imaginary/paraffins/Main.hs.opt &&
+stack exec exp ~/nofib/imaginary/primes/Main.hs.opt &&
+stack exec exp ~/nofib/imaginary/queens/Main.hs.opt &&
+stack exec exp ~/nofib/imaginary/rfib/Main.hs.opt &&
+stack exec exp ~/nofib/imaginary/tak/Main.hs.opt &&
+stack exec exp ~/nofib/imaginary/wheel-sieve1/Main.hs.opt &&
+stack exec exp ~/nofib/imaginary/wheel-sieve2/Main.hs.opt &&
+stack exec exp ~/nofib/imaginary/x2n1/Main.hs.opt 
